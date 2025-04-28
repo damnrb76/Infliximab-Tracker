8663313c1304343c58011b7435fc51f13c1b4b68
damnrb76 <damonrb76@gmail.com>
2025-04-28 22:31:49 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+// service-worker.js
+
+const CACHE_NAME = 'infliximab-tracker-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Fetch Resources
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
